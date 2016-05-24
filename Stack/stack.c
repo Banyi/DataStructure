e1ab39814a7ffabdfe264ac3f1cae2646601d35e
Banyi <1181395975@qq.com>
2016-05-24 23:43:36 +0800

    Update stack.c

--- Stack/stack.c
+++ Stack/stack.c
@@ -1,105 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-
-#define  STACK_INIT_SIZE   100     //存储空间初始分配量 
-#define  STACKINCREMENT     10     //存储空间增量 
-
-
-/*状态码*/
-#define  TRUE         1
-#define  FALSE        0
-#define  OK           1
-#define  YES          1
-#define  NO           0
-#define  ERROR        0
-#define  SUCCESS      1
-#define  OVERFLOW    -2
-
-
-typedef int Status;
-typedef int SElemType; 
-
-typedef struct
-{
-	SElemType *base;        //在栈构造之前和销毁之后，base的值为NULL 
-	SElemType *top;         //栈顶指针 
-	int stacksize;          //当前已分配的存储空间，以元素为单位 
-}SqStack;
-
-/*构造空栈*/
-Status InitStack();
-
-/*销毁栈*/ 
-Status DestroyStack();
-
-/*清空栈*/ 
-Status ClearStack();
-
-/*判断栈是否为空*/ 
-Status StackEmpty();
-
-/*返回栈的长度*/ 
-int StackLength();
-
-/*获取栈顶*/ 
-Status GetTop();
-
-/*插入新元素*/
-Status Push();
-
-/*删除栈顶元素*/ 
-Status Pop();
-
-Status StackTraverse();
-
-Status visit();
-
-void test();
-
-int main()
-{
-    int n = 1234;
-    printf("%d\n", n%10);
-	
-	return 0;
-}
-
-void test()
-{
-	SqStack S;
-	SElemType e, h;
-	
-	//初始化栈
-	InitStack(&S);
-	
-	//判空
-	StackEmpty(S) ? printf("栈为空栈！\n") : printf("栈不为空栈！\n");
-	
-	//将e压入栈
-	e = 10;
-	h = 12;
-	Push(&S, e);
-	Push(&S, h);
-	StackTraverse(S, visit);
-	StackEmpty(S) ? printf("栈为空栈！\n") : printf("栈不为空栈！\n");
-	
-	//获取栈顶元素
-	GetTop(S, &e);
-	printf("栈顶元素为：%d\n", e);
-	Push(&S, 15);
-	GetTop(S, &e);
-	printf("栈顶元素为：%d\n", e);
-	printf("栈长度为：%d\n", StackLength(S));
-	
-	Push(&S, 16);
-	StackTraverse(S, visit);
-	
-	//把栈顶弹出
-	Pop(&S, &e);
-	printf("弹出栈的元素为：%d\n", e);
-	StackTraverse(S, visit);	
-}
+#ifndef STACK_C
+#define STACK_C
+#include "stack.h"
 
 /*1.构造空栈*/
 Status InitStack(SqStack *S) 
@@ -214,3 +115,5 @@ Status visit(SElemType e)
 	return OK;
 	
 }
+
+#endif
